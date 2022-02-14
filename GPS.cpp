17cd1af55b196fb0d3ee95ebdbc278279b0ddadb
liuchuanstudy <535496830@qq.com>
2022-02-14 20:51:29 +0800

    Add EKF to filter the course angle

--- GPS.cpp
+++ GPS.cpp
@@ -28,6 +28,7 @@ unsigned char hexToDec(unsigned char hex)
 
 GPS::GPS()
 {
+	valid = false;
 }
 
 
@@ -88,8 +89,140 @@ void GPS::parseNAME(string parseStr)
 					pos = strs[5].find('.');
 					if(pos != string::npos)
 						lon = atof(strs[5].substr(0, 3).c_str()) + atof(strs[5].substr(pos - 2).c_str()) / 60;
-					velocity = atof(strs[7].c_str());
-					course = atof(strs[8].c_str());	
+					velocity = atof(strs[7].c_str())*0.5144;
+					course = atof(strs[8].c_str());
+				}
+				else if (strs[0] == "$GNGGA")
+				{
+#ifdef GPSDEBUG
+					cout << "gotGNGGA" << endl;
+#endif // GPSDEBUG
+					int pos = strs[2].find('.');
+					if(pos != string::npos)
+						lat = atof(strs[2].substr(0, 2).c_str()) + atof(strs[2].substr(pos - 2).c_str()) / 60;
+					pos = strs[4].find('.');
+					if(pos != string::npos)
+						lon = atof(strs[4].substr(0, 3).c_str()) + atof(strs[4].substr(pos - 2).c_str()) / 60;
+					SVs = atoi(strs[7].c_str());
+					HDOP = atof(strs[8].c_str());
+					altitude = atof(strs[9].c_str());
+				}
+			}
+		}
+	}
+}
+
+/* Description: initialize EKF. 
+ * Regard the current position (from GNSS) and yaw (from IMU) as the init state of EKF;
+ * @param yaw(deg) The heading angle of vehicle(May obtain from IMU).
+ */
+void GPS::initEKF(double yaw)
+{
+	x_hat << lat/d2r,lon/d2r,0,yaw/d2r,0;
+	P_hat = Matrix5d::Identity() * 1e-3;
+	
+	oldTime = micros();
+}
+
+/* Descripiton: A five state EKF to filter the course angle
+ * State: [lat(rad),lon(rad),velocity(m/s),chi(rad),r(rad)];
+ * Measurement: [lat_GNSS(rad),lon_GNSS(rad),vel_GNSS(rad),r(IMU)];
+ * Reference: Five-state extended kalman filter for estimation of speed over ground (SOG), course over ground (COG), and course rate of unmanned surface vehicles (USVs): Experimental results
+ * 			 (DOI: 10.3390/s21237910)
+ * @param: r(deg/s)
+ */
+void GPS::gnssEKF(double r)
+{
+	newTime = micros();
+	deltaT = (newTime-oldTime)*0.001*0.001;
+	
+	double midValue = 1-ee*sin(x_hat(2))*sin(x_hat(2));
+	double Rn = a/sqrt(midValue);
+	double Rm = Rn*(1-ee)/midValue;
+	
+	Vector5d f(x_hat(3)*cos(x_hat(4))/Rm,
+			   x_hat(3)*sin(x_hat(4))/Rn/cos(x_hat(1)),
+			   -alpha_1*x_hat(3),
+			   x_hat(3),
+			   -alpha_2*x_hat(5));
+	Matrix5d Acom{{0,0,cos(x_hat(4))/Rm,-x_hat(3)*sin(x_hat(4))/Rm,0},
+				  {x_hat(3)*sin(x_hat(4))*tan(x_hat(1))/Rn/cos(x_hat(1)),0,sin(x_hat(4))/Rn/cos(x_hat(1)),x_hat(3)*cos(x_hat(4))/Rn/cos(x_hat(1)),0},
+				  {0,0,-alpha_1,0,0},
+				  {0,0,0,0,1},
+				  {0,0,0,0,-alpha_2}};
+	
+	Matrix5d A = Matrix5d::Identity()+deltaT*Acom;
+	Vector5d x_prd = x_hat+ deltaT * f;
+	x_prd(4) = satCourse(x_prd(4));
+	
+	Matrix5d p_prd = A*P_hat*A.transpose()+E*Q*E.transpose();
+	
+	Vector4d y(lat/d2r,lon/d2r,velocity,r/d2r);
+	Matrix<double,5,4> K = p_prd*C.transpose()*((C*p_prd*C.transpose()+R).inverse());
+	P_hat = (Matrix5d::Identity()-K*C)*p_prd;
+	x_hat = x_prd+K*(y-C*x_prd);
+	x_hat(4) = satCourse(x_hat(4));
+	
+	oldTime = newTime;
+}
+
+double GPS::satCourse(double course)
+{
+	while(course<0)
+	{
+		course+=2*M_PI;
+	}
+	while(course>2*M_PI)
+	{
+		course-=2*M_PI;
+	}
+	return course;
+}
+/* Description: Parse NAME and run EKF if get valid position infomation;
+ * Call this functuon must after GPS::initEKF.
+ * @param parseStr The original string from serial or network.
+ * @param r The rate of yaw in uints deg/s
+ */
+void GPS::parseNAME(string parseStr, double r)
+{
+	int parseLength = 1;
+	int len = static_cast<int>(parseStr.length());
+	
+	while (len > parseLength)
+	{
+		string str = parseStr.substr(parseLength - 1);
+#ifdef GPSDEBUG
+		cout << str << endl;
+#endif // GPSDEBUG
+		if (parse(str, parseLength))  //成立则获取到一条完整的NMEA消息
+		{
+			if (checkCRC()) //进行CRC校验，校验该条NMEA消息数据是否准确。
+			{
+				vector<string> strs = testSplit(payload, ",");
+#ifdef GPSDEBUG
+				cout << "goodCRC" << "  "<< strs.size() << endl;
+#endif // GPSDEBUG
+				//若校验通过，以下代码提取经纬度，航速和航向信息
+				if (strs[0] == "$GNRMC") //"$GNRMC":GNSS推荐定位消息；
+				{
+#ifdef GPSDEBUG
+				cout << "gotGNRMC" << endl;
+#endif // GPSDEBUG
+					if(strcmp(strs[2].c_str(),"A") == 0)
+					{
+						valid = true;
+						int pos = strs[3].find('.');
+						if(pos != string::npos)
+							lat = atof(strs[3].substr(0, 2).c_str()) + atof(strs[3].substr(pos - 2).c_str()) / 60;
+						pos = strs[5].find('.');
+						if(pos != string::npos)
+							lon = atof(strs[5].substr(0, 3).c_str()) + atof(strs[5].substr(pos - 2).c_str()) / 60;
+						velocity = atof(strs[7].c_str())*0.5144;
+						course = atof(strs[8].c_str());
+						gnssEKF(r);
+					}
+					else
+						valid = false;
 				}
 				else if (strs[0] == "$GNGGA")
 				{
--- GPS.h
+++ GPS.h
@@ -1,14 +1,20 @@
 #ifndef GPS_H
 #define GPS_H
 
-#include<string>
-#include<iostream>
-#include<vector>
+#include <string>
+#include <iostream>
+#include <vector>
+#include <Eigen/Dense>
+#include "../ArduTime.h"
 
 using std::vector;
 using std::string;
 using std::cout;
 using std::endl;
+
+using namespace Eigen;
+typedef Matrix<double, 5, 5> Matrix5d;
+typedef Matrix<double, 5, 1> Vector5d;
 
 vector<string> testSplit(string srcStr, const string& delim);
 unsigned char hexToDec(unsigned char hex);
@@ -19,20 +25,52 @@ public:
 	GPS();
 	~GPS();
 	void parseNAME(string);
+	void parseNAME(string,double);
+	void initEKF(double yaw);
 	
 	bool valid;
 	double lat, lon;
 	float velocity, course, altitude;
 	uint8_t SVs;
 	float HDOP;
+	
+	double getEKFLat() const {return x_hat(0)*d2r;};
+	double getEKFLon() const {return x_hat(1)*d2r;};
+	double getEKFVel() const {return x_hat(2);};
+	double getEKFCourse() const{return x_hat(3)*d2r;};
+	double getEKFr() const{return x_hat(4)*d2r;};
 
 private:
 	const string header = "$GN";  
 	const string footer = "\r\n";
 	string payload;
-
+	
     bool parse(string str, int &parseLen);
 	bool checkCRC();
+	
+	const double a = 6378137;
+	const double ee = 0.00669437999;
+	const double d2r = 57.2957795130;
+	
+	/*GNSS EKF*/
+	double deltaT{0.};
+    uint32_t newTime{0}, oldTime{0};
+	Vector5d x_hat;
+	float alpha_1 = 0.01;
+	float alpha_2 = 0.1;
+	Matrix5d P_hat = Matrix5d::Identity() * 0.01;
+    Matrix2d Q{{5,0},{0,1}};
+	Matrix4d R{{1e-14,0,0,0},
+			   {0,1e-14,0,0},
+			   {0,0,0.2,0},
+			   {0,0,0,0.001}};
+	Matrix<double,4,5> C{{1,0,0,0,0},
+						 {0,1,0,0,0},
+						 {0,0,1,0,0},
+						 {0,0,0,0,1}};
+	Matrix<double,5,2> E{{0,0},{0,0},{1,0},{0,0},{0,1}};
+	void gnssEKF(double r);
+	double satCourse(double course);
 };
 
 #endif
