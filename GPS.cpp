ff9ad4e156f5a5ee0b312aca870df1fc095911ce
liuchuanstudy <535496830@qq.com>
2022-02-14 20:54:21 +0800

    Modify note

--- GPS.cpp
+++ GPS.cpp
@@ -126,7 +126,7 @@ void GPS::initEKF(double yaw)
 
 /* Descripiton: A five state EKF to filter the course angle
  * State: [lat(rad),lon(rad),velocity(m/s),chi(rad),r(rad)];
- * Measurement: [lat_GNSS(rad),lon_GNSS(rad),vel_GNSS(rad),r(IMU)];
+ * Measurement: [lat_GNSS(rad),lon_GNSS(rad),vel_GNSS(rad),r_IMU(rad)];
  * Reference: Five-state extended kalman filter for estimation of speed over ground (SOG), course over ground (COG), and course rate of unmanned surface vehicles (USVs): Experimental results
  * 			 (DOI: 10.3390/s21237910)
  * @param: r(deg/s)
